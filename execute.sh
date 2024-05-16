21d9c32cef981027cf1f495a39e3a6be171024fa
americoleonardo <leonardoakeisman@gmail.com>
2024-05-16 20:10:40 -0300

    wip: build

--- execute.sh
+++ execute.sh
@@ -3,8 +3,6 @@ echo "Update Environment"
 
 cat .crds-to-apply | grep "environments/dev" > crds-env-to-apply.txt | true
 
-# Read the input file line by line using a for loop
-IFS=$'\n' # set the Internal Field Separator to newline
 for line in $(cat "crds-env-to-apply.txt")
 do
     command="ssd apply -f $line"
